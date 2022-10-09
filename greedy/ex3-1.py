7202caea6a76ea94ef04f3cfb6d65d1ea6efb3f5
yujin <yujin@naver.com>
2022-10-09 14:30:35 +0900

    큰 수의 법칙

--- greedy/ex3-1.py
+++ greedy/ex3-1.py
@@ -2,22 +2,14 @@
 # 거슬러 줘 할 돈이 N원(N은 항상 10의 배수)
 # 거슬러줘야 할 동전의 최소 개수는?
 
-balance = 1600
+balance = 200
 count = 0
 
-while balance > 0:
-  if balance >= 500:
-    count += balance // 500
-    balance %= 500
-  elif balance >= 100:
-    count += balance // 100
-    balance %= 100
-  elif balance >= 50:
-    count += balance // 50
-    balance %= 50
-  elif balance >= 10:
-    count += balance // 10
-    balance %= 10
+# 큰 단위의 화폐부터 차례대로 확인
+list = [500, 100, 50, 10]
 
-print("count: ", count)
-print("balance: ", balance)
+for coin in list:
+    count += balance // coin #해당 화폐로 거슬러 줄 수 있는 동전의 개수 세기
+    balance %= coin
+
+print(count)
---
+++ greedy/ex3-2.py
@@ -0,0 +1,25 @@
+"""
+배열의 크기 : N,
+숫자가 더해지는 횟수 : M,
+숫자를 연속해서 적을 수 있는 횟수 : K
+
+첫째 줄에 N(2<= N <= 1000), M(1<= M <= 10000), K(1<=k<=10000)의 자연수가 주어지며, 각 자연수는 공백으로 구분한다.
+둘째 줄에 N개의 자연수가 주어진다. (1이상 10000이하의 수로 주어짐)
+입력으로 주어지는 K는 항상 M보다 작거나 같다.
+"""
+N, M, K = map(int,input().split())
+
+data = list(map(int, input().split()))
+
+data.sort(reverse=True)
+
+result2 = data[0] * K + data[1]
+
+count = M//(K+1)
+count2 = M%(K+1)
+
+result = 0
+result += result2 * count
+result += data[0] * count2
+
+print(result)
