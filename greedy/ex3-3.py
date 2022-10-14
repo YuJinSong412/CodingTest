96a7f1cc470cf9102181d37002d4d4c1a055d3a0
yujin <yujin@naver.com>
2022-10-14 17:26:33 +0900

    3-3

--- greedy/ex3-3.py
+++ greedy/ex3-3.py
@@ -7,3 +7,19 @@ for i in range(n):
   result = max(result, min_value)
 
 print(result)
+
+# n, m = map(int, input().split())
+
+# arr = [list(map(int, input().split())) for i in range(n)]
+# # 2차원 입력 방법 기억해두기.
+
+# mini = 1
+# maxi = 1
+# # 선언할 때 함수이름으로 변수명 하지 않도록 조심하자.
+
+# for i in range(n):
+#     mini = min(arr[i])
+#     if maxi < mini:
+#         maxi = mini
+
+# print(maxi)
