d253eb28ec07727eef1b86dcb799f5e181017b48
yujin <yujin@naver.com>
2022-10-28 19:12:17 +0900

    chapter3

---
+++ grammar/chapter3/Q2.py
@@ -0,0 +1,9 @@
+result = 0
+i = 0
+
+while i < 1000:
+    i += 1
+    if(i % 3 == 0):
+        result += i
+
+print(result)
---
+++ grammar/chapter3/Q3.py
@@ -0,0 +1,11 @@
+i = 0
+
+# while i < 5:
+#     i += 1
+#     result = "*" * i
+#     print(result)
+
+while True:
+    i += 1
+    if i > 5 : break
+    print("*"*i)
